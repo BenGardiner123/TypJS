405312e5866bd4da9df9201e381465c8ef151edc
Ben Gardiner <bennygards@gmail.com>
2020-05-10 22:29:58 +1000

    This is the setup we're using going fwd

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-let myButton = document.getElementById("myButton");
+let myButton = document.getElementById("myButton") as HTMLButtonElement | null;
+if(myButton == null) {
+    alert ("button not found!");
+} else {
+    myButton.value
+    myButton.onclick = function(){
+        if(myButton == null) return;
+        alert(myButton.value);
+    }
+}
